cfb512ffa353c5dde257f26adfece7edf00bb842
HScarb <jjhfen00@163.com>
2017-03-14 20:39:11 +0800

    Update spider 1. Add dict.

--- 01-current-movie.py
+++ 01-current-movie.py
@@ -45,7 +45,16 @@ def crawMovie(movieID):
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
         }
     text = ''
-    movieDataDict = {}
+    movieDataDict = {'cname':None,
+                     'ename':None,
+                     'type':None,
+                     'length':None,
+                     'releasetime':None,
+                     'standard':None,
+                     'director':None,
+                     'productioncompany':None,
+                     'publisher':None,
+                     'sumboxoffice':None}
     print('Crawing movie info ', url)
     # 抓取整个网页
     try:
@@ -57,11 +66,47 @@ def crawMovie(movieID):
     soup = BeautifulSoup(text, "lxml")
     result1 = soup.find('div', class_='cont')               # 标题等信息
     result2 = soup.find('dl', class_='dltext')
+    result2 = result2.find_all('dd')
     #print(result1.h2.text)
+    i = 0
+    isSumBoxOffice = False
     for element in result1.stripped_strings:
-        print(element.replace('\r','').replace('\n','').replace(' ', ''))
-    for element in result2.stripped_strings:
-        print(element)
+        if isSumBoxOffice == True:
+            movieDataDict['sumboxoffice'] = int(float(element[0:-1]) * 10000)
+            isSumBoxOffice = False
+        elif i == 0:
+            movieDataDict['cname'] = element
+        elif i == 2:
+            movieDataDict['ename'] = element
+        elif element[0] == '类':
+            movieDataDict['type'] = element[3:]
+        elif element[0] == '片':
+            movieDataDict['length'] = element.replace('\r','').replace('\n','').replace(' ', '')[3:-3]
+        elif element[0] == '上':
+            dateStr = element[5:-6]
+            # 需要处理字符串成日期的形式
+            movieDataDict['releasetime'] = element[5:-4]
+        elif element[0] == '制':
+            movieDataDict['standard'] = element[3:]
+        elif element[0] == '累':
+            isSumBoxOffice = True
+        i = i + 1
+
+    i = 0
+    print(movieDataDict)
+    for element in result2:
+        '''
+        i = 0: 导演
+            1: 主演
+            2: 制作公司
+            3: 发行公司
+        '''
+        links = element.find_all('a')
+        for link in links:
+            print(link)
+
+        i = i + 1
+
     pass
 
 def crawActor(actorID):
