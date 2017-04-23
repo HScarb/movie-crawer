bd5aeb8d7aba75ccf7c8a5bf941697a5bceb39cc
oneone1995 <wanglixian1995@live.com>
2017-04-23 16:36:20 +0800

    获取城市详细拍片脚本中新增爬取该影片有排片的城市列表

---
+++ m15_city_movie_schedule.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import requests
+import re
+
+# 默认等待时间
+DEFAULT_TIMEOUT = 10
+
+
+# 获取城市列表的爬虫
+def craw_city_list(movie_id):
+    url = 'http://pp.58921.com/film/' + str(movie_id)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/48.0.2564.116 Safari/537.36 '
+    }
+    html_text = ''
+    # 抓取整个网页
+    try:
+        print('Requesting url: ', url)
+        html_text = requests.get(url, headers=headers, timeout=DEFAULT_TIMEOUT).text
+    except:
+        print('Error when request url=', url)
+        return None
+
+    # 编码修改，防止出现中文乱码
+    html_text = html_text.encode('latin1').decode('utf-8')
+
+    # 获取城市url列表
+    city_list = re.findall('<a href="(/film/.*?)" .*?>', html_text)
+    return city_list
+
+
+def main():
+    craw_city_list(6411)
+
+
+if __name__ == '__main__':
+    main()
