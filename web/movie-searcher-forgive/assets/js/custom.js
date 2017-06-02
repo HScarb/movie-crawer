b5c92e79661f88c4ccfa4d6470ac860c244fe628
Scarb <jjhfen00@163.com>
2017-06-02 16:10:46 +0800

    Add front-end.

---
+++ web/movie-searcher-forgive/assets/js/custom.js
@@ -0,0 +1,41 @@
+﻿
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+       
+        reviews_fun:function()
+        {
+            ($)(function () {
+                $('#carousel-example').carousel({
+                    interval: 3000 //TIME IN MILLI SECONDS
+                });
+            });
+
+        },
+     
+        custom_fun:function()
+        {
+
+
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.reviews_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+
